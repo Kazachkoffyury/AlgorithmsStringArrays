7059eedad8f61f1a284f8670ebd8f36fd4e5fe95
Kazackoffyury <kazachkoff.yury@yandex.ru>
2022-01-28 18:12:40 +0300

    fix#1

--- src/ru/skypro/StringListImpl.java
+++ src/ru/skypro/StringListImpl.java
@@ -1,83 +1,129 @@
 package ru.skypro;
 
+import java.util.Arrays;
+
 public class StringListImpl implements StringList{
-    private String [] box;
+    private String[] storage;
     private int size;
 
-
-    public StringListImpl () {
-        box = new String[10];
-        size = 0;
+    public StringListImpl() {
+        this.storage = new String[10];
     }
+
     @Override
     public String add(String item) {
-
-       return item;
+        if (size >= storage.length) {
+            storage = Arrays.copyOf(storage, (int) (storage.length + (storage.length * 0.5)));
+        }
+        storage[size++] = item;
+        return item;
     }
 
     @Override
     public String add(int index, String item) {
-
+        if (index >= size || index < 0) {
+            throw new InvalidIndexException();
+        }
+        System.arraycopy(storage, index, storage, index + 1, size - index);
+        storage[index] = item;
+        size++;
         return item;
     }
 
     @Override
     public String set(int index, String item) {
-        return null;
+        if (index >= size || index < 0) {
+            throw new InvalidIndexException();
+        }
+        storage[index] = item;
+        return item;
     }
 
     @Override
     public String remove(String item) {
-        return null;
+        int itemIndex = indexOf(item);
+        if (itemIndex == -1) {
+            throw new ItemNotFoundException();
+        }
+        if (itemIndex != storage.length - 1) {
+            System.arraycopy(storage, itemIndex + 1, storage, itemIndex, size - itemIndex);
+        }
+        size--;
+        return item;
     }
 
     @Override
     public String remove(int index) {
-        return null;
+        if (index >= size || index < 0) {
+            throw new InvalidIndexException();
+        }
+        String itemToRemove = storage[index];
+        if (index != storage.length - 1) {
+            System.arraycopy(storage, index + 1, storage, index, size - index);
+        }
+        size--;
+        return itemToRemove;
     }
 
     @Override
     public boolean contains(String item) {
-        return false;
+        int itemIndex = indexOf(item);
+        return itemIndex >= 0;
     }
 
     @Override
     public int indexOf(String item) {
-        return 0;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].equals(item)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public int lastIndexOf(String item) {
-        return 0;
+        for (int i = size - 1; i >= 0; i--) {
+            if (storage[i].equals(item)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public String get(int index) {
-        return null;
+        if (index <= size && index >= 0) {
+            return storage[index];
+        }
+        throw new InvalidIndexException();
     }
 
     @Override
     public boolean equals(StringList otherList) {
-        return false;
+        if (otherList == null) {
+            throw new NullParameterException();
+        }
+        return Arrays.equals(toArray(), otherList.toArray());
     }
 
     @Override
-    public int size() {
-        return 0;
+    public int getSize() {
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return size ==0;
+        return size == 0;
     }
 
     @Override
     public void clear() {
-
+        size = 0;
     }
 
     @Override
     public String[] toArray() {
-        return new String[0];
+        return Arrays.copyOf(storage, size);
     }
 }
